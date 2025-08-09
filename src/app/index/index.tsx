fe26c557340ea774a0746772cbbc0965980c643d
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-08-09 18:52:33 -0300

    feat: add home page & some configs

---
+++ src/app/index/index.tsx
@@ -0,0 +1,18 @@
+import { Image, View, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { styles } from "./styles"
+import { colors } from "@/styles/colors"
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+                <TouchableOpacity activeOpacity={0.8}>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
